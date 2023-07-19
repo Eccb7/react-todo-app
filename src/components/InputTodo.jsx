8fcfc71365539b99fdcb565bed6227964d78fd21
Briton Otieno <ojwangbriton@gmail.com>
2023-07-19 17:35:05 +0300

    Add app component files

---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+const InputTodo = ({ addTodoItem }) => {
+  const [title, setTitle] = useState('');
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodoItem(title);
+    console.log(title);
+    setTitle('');
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="form-container">
+        <input
+          type="text"
+          placeholder="Add Todo..."
+          value={title}
+          onChange={handleChange}
+          className="input-text"
+        />
+        <button className="input-submit">Submit</button>
+      </form>
+      <span className="submit-warning" />
+    </>
+  );
+};
+export default InputTodo;
